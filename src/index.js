8b35a3c58a285e995347834f38eae70799e9d1ec
azazelo <atvai@inbox.ru>
2022-02-20 19:34:38 +0300

    feat:creat new js file

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    if ( (typeof matrix==="undefined" )|| (matrix.length===0 ) ){
+        return []
+      }
+    else {
+    const arr=matrix.map(function(name){
+        if(Array.isArray(name) && (matrix.indexOf(name)+1)%2===0){
+            name.sort(function(a,b){return b-a})
+            return name.join()
+        }
+        else if(Array.isArray(name) && (matrix.indexOf(name)+1)%2!==0){
+          return name.join()
+        }
+        return name
+        });
+        const tin=arr.join(',');
+      const er=tin.split(',');
+    return er;
+      }
+
+  }
+
